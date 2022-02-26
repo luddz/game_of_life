003f1c0a7e7a9137aaa1c763a9b071360a4d6b67
Ludvig Kindberg <chandazz96@gmail.com>
2022-02-26 23:24:08 +0100

    ADD: test cases for common start positions for stil lifes and oscillators

--- rules.py
+++ rules.py
@@ -21,27 +21,24 @@ def rule_three(alive_neighbours:int, is_alive:bool) -> bool:
 def rule_four(alive_neighbours:int, is_alive:bool) -> bool:
     return not is_alive and alive_neighbours == 3
 
+def check_alive_cell(alive_neighbours:int, is_alive:bool) -> bool:
+    return rule_one(alive_neighbours, is_alive) and rule_two(alive_neighbours, is_alive) and rule_three(alive_neighbours, is_alive)
+
 def check_rules(prev_matrix, position) -> bool:
     alive_neighbours = 0
     for i in range(-1, 2):
         for j in range(-1, 2):
-            
             test_x = position[0] + i
             test_y = position[1] + j
-            # Check so we are not checking our own position
-            # Out of bounds checks.
+            # Check so we are not checking our own position and OB checks.
             if (test_x, test_y) == position  or test_x < 0 or test_x >= prev_matrix.shape[0] or test_y < 0 or test_y >= prev_matrix.shape[1]:
                 continue
     
             if prev_matrix[(test_x, test_y)] == 1:
                 alive_neighbours += 1
 
-    is_alive = prev_matrix[(position)] == 1
-    # print(f"\nResult rule_one: {rule_one(alive_neighbours, is_alive)}")
-    # print(f"Result rule_two: {rule_two(alive_neighbours, is_alive)}")
-    # print(f"Result rule_three: {rule_three(alive_neighbours, is_alive)}")
-    # print(f"Result rule_four: {rule_four(alive_neighbours, is_alive)} \n")
-    return rule_one(alive_neighbours, is_alive) or rule_two(alive_neighbours, is_alive) or rule_three(alive_neighbours, is_alive) or rule_four(alive_neighbours, is_alive)
+    is_alive = prev_matrix[(position)] == 1 
+    return check_alive_cell(alive_neighbours, is_alive) if is_alive else rule_four(alive_neighbours, is_alive)
 
 
 class RulesTesting:
@@ -54,17 +51,115 @@ class RulesTesting:
 
         return next_board
 
+    def run_all_tests(self) -> None:
+        tests = []
+        tests.append(("Empty", self.test_empty_board()))
+        tests.append(("Single point", self.test_single_point()))
+        tests.append(("Two points seperated", self.test_two_points_seperated()))
+        tests.append(("Two points together", self.test_two_points_together()))
+        tests.append(("Block", self.test_block()))
+        tests.append(("Bee hive", self.test_bee_hive()))
+        tests.append(("Loaf", self.test_loaf()))
+        tests.append(("Boat", self.test_boat()))
+        tests.append(("Tub", self.test_tub()))
+        tests.append(("Blinker", self.test_blinker()))
+        tests.append(("Beacon", self.test_beacon()))
+
+        for name, res in tests:
+            p, f  = "Passed", "Failed"
+            print(f"{p if res else f} test {name}")
+
+    def test_static_from_start_pos(self, start_poses):
+        start_board = np.copy(self.zero_board)
+        for pos in start_poses:
+            start_board[pos] = 1
+        return np.array_equal(start_board ,  self.run_one_step(start_board))
+    
+    def test_transformation_from_start_to_final(self, start_poses, final_poses):
+        start_board, final_board = np.copy(self.zero_board), np.copy(self.zero_board)
+        for pos in start_poses:
+            start_board[pos] = 1
+        
+        for pos in final_poses:
+            final_board[pos] = 1
+        
+        return np.array_equal(final_board , self.run_one_step(start_board))
+
+    def test_block(self) -> bool:
+        start_poses = [(5,5), (6,5), (5,6), (6,6)]
+        return self.test_static_from_start_pos(start_poses=start_poses)
+
+    def test_bee_hive(self) -> bool:
+        # start_poses = [(5,5), (6,5), (4,6), (7,6), (5,7), (6,7)] #Column major
+        start_poses =[(5,5), (5,6), (6,4), (6,7), (7,5), (7,6)] # Row Major
+        return self.test_static_from_start_pos(start_poses=start_poses)
+
+    def test_loaf(self) -> bool:
+        start_poses = [
+            (3,5),(3,6),
+            (4,4),(4,7),
+            (5,5),(5,7),
+            (6,6)]
+        return self.test_static_from_start_pos(start_poses=start_poses)
+
+    def test_boat(self) -> bool:
+        start_poses = [
+            (4,4), (4,5),
+            (5,4), (5,6),
+            (6,5)
+        ]
+        return self.test_static_from_start_pos(start_poses=start_poses)
+
+    def test_tub(self) -> bool:
+        start_poses = [
+            (4,5),
+            (5,4), (5,6),
+            (6,5)
+        ]
+        return self.test_static_from_start_pos(start_poses=start_poses)
+    
+    def test_blinker(self) -> bool:
+        start_poses = [(4,5), (5,5), (6,5)]
+        final_poses = [(5,4), (5,5), (5,6)]
+        return self.test_transformation_from_start_to_final(start_poses, final_poses)
+
+    def test_beacon(self) -> bool:
+        start_poses = [
+            (4,4), (4,5),
+            (5,4), (5,5),
+            (6,6), (6,7),
+            (7,6), (7,7)]
+        final_poses = [
+            (4,4), (4,5),
+            (5,4), 
+            (6,7),
+            (7,6), (7,7)]
+        return self.test_transformation_from_start_to_final(start_poses, final_poses)
+
+    def test_two_points_together(self) -> bool:
+        start_board = np.copy(self.prev_board)
+        start_board[(5,5)] = 1
+        start_board[(5,6)] = 1
+        next = self.run_one_step(start_board)
+        return np.array_equal(self.zero_board, next)
+
+    def test_two_points_seperated(self) -> bool: 
+        start_board = np.copy(self.prev_board)
+        start_board[(2,2)] = 1
+        start_board[(8,8)] = 2
+        next = self.run_one_step(start_board)
+        return np.array_equal(self.zero_board, next)
+
     def test_single_point(self) -> bool:
-        start_board = np.zeros((self.width, self.height))
+        start_board = np.copy(self.prev_board)
         start_pos = (5,5)
         start_board[start_pos] = 1
         next_board = self.run_one_step(start_board)
-        return np.equal(self.zero_board, next_board).all()
+        return np.array_equal(self.zero_board, next_board)
 
     def test_empty_board(self) -> bool:
-        start_board = np.zeros((self.width, self.height))
-        next_board = self.run_one_step(start_board)
-        return np.equal(self.zero_board, next_board).all()
+        next_board = self.run_one_step(self.prev_board)
+        return np.array_equal(self.zero_board, next_board)
 
     def __init__(self) -> None:
         self.width = 10
@@ -76,11 +171,4 @@ class RulesTesting:
 
 if __name__ == '__main__':
     test = RulesTesting()
-    test_results = []
-    test_results.append(test.test_empty_board())
-    test_results.append(test.test_single_point())
-
-    for n, res in enumerate(test_results):
-        p = "Passed"
-        f = "failed"
-        print(f"Test {n} was {p if res else f}")
+    test.run_all_tests()
